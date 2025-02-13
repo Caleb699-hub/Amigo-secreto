212387df6858d6fe7d6a00af79ef9cb69e59c0e1
caleb <daniellesnow09@gmail.com>
2025-02-13 09:02:17 -0500

    actualiza el app.js, se crea el array que contiene losnombres de los amigos y se crea la funcion que valida el input y agrega el valor al array

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresIngresados =[];
+console.log(nombresIngresados.length);
+
+
+function agregarAmigo(){
+        let entrada = document.getElementById("amigo").value;
+        let valorEntrada = entrada.trim();
+        if(valorEntrada ===""){
+            alert("Por favor, inserte un nombre");
+        }else{
+            nombresIngresados.push(valorEntrada);
+            document.getElementById("amigo").value="";
+        }
+    console.log(nombresIngresados);
+
+}
